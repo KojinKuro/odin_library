19671ad05df180c6d519ca62cf696274370e3e5d
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-01-22 02:05:09 -0800

    Allow books dynamic adding
    Books can now be dynamically added through calling the displayBooks()
    function. Books must be added through addBookToLibrary().

--- script.js
+++ script.js
@@ -11,5 +11,23 @@ function addBookToLibrary(name) {
 }
 
 function displayBooks() {
-  
+  const booksNode = document.querySelector('#books-container');
+  myLibrary.forEach((book) => {
+    console.log(book);
+    const bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "book-card");
+    // add title
+    const bookName = document.createElement("div");
+    bookName.setAttribute("class", "book-name");
+    const titleText = document.createTextNode(book.name);
+    bookName.appendChild(titleText);
+    bookCard.appendChild(bookName);
+    //add image
+    const bookImage = document.createElement("img");
+    bookImage.setAttribute("src", "./images/book.jpeg");
+    bookImage.setAttribute("alt", "Some dynamic alt text");
+    bookCard.appendChild(bookImage);
+    // stick to the container
+    booksNode.appendChild(bookCard);
+  });
 }
