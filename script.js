0ce4749dce5ed924fb0b06b9edf8748b34dd476f
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-19 09:57:34 -0800

    Convert into classes and a module pattern
    The code has bee cleaned up to group similar functions together into
    3 groups rather than one big group as before. There is now a class
    for the book and it's related code, a class for library and it's related
    code, and a domHandler module for dealing with the DOM. The code should
    be more readable.

--- script.js
+++ script.js
@@ -1,18 +1,88 @@
+class Book {
+  constructor(name, author, pages, status, rating) {
+    if (this.constructor.isBlank(name)) this.name = "Unknown Book";
+    else this.name = name;
+
+    if (this.constructor.isBlank(author)) this.author = "Some Author";
+    else this.author = author;
+
+    if (this.constructor.isBlank(pages)) this.pages = "?";
+    else this.pages = pages;
+
+    if (this.constructor.isBlank(name)) this.status = "reading";
+    else this.status = status;
+
+    // does not need data validation, will always have input
+    this.rating = rating;
+  }
+
+  static isBlank(string) {
+    return string === "";
+  }
+}
+
+class Library {
+  #myLibrary = [];
+
+  constructor(name = "library") {
+    this.name = name;
+  }
+
+  addBook(book) {
+    this.#myLibrary.push(book);
+    this.saveLibrary();
+    domHandler.displayBooks();
+  }
+
+  length = () => this.#myLibrary.length;
+
+  library = () => this.#myLibrary;
+
+  getBook(index) {
+    return this.#myLibrary.at(+index);
+  }
+
+  removeBook(index) {
+    this.#myLibrary.splice(index, 1);
+    this.saveLibrary();
+    domHandler.displayBooks();
+  }
+
+  editBook(index, newBook) {
+    this.#myLibrary.splice(index, 0, newBook);
+    this.#myLibrary.splice(++index, 1);
+  }
+
+  saveLibrary() {
+    localStorage.setItem(this.name, JSON.stringify(this.library()));
+  }
+
+  loadLibrary() {
+    let libraryVal = JSON.parse(localStorage.getItem(this.name));
+    if (!libraryVal) return;
+    libraryVal.forEach((book) => this.addBook(book));
+  }
+
+  static addFluffBooks(library) {
+    library.addBook(new Book("The Elixir", "Penelope Puzzleton", 352, "false"));
+    library.addBook(new Book("Whispers of Wind", "Jasper Moran", 214, "false"));
+    library.addBook(new Book("Shadow's Crest", "Mira Light", 198, "true"));
+    library.addBook(new Book("Echoes in Time", "Arthur Gray", 289, "false"));
+    library.addBook(new Book("Frostbound", "Evelyn Storm", 367, "true"));
+  }
+}
+
+let myLibrary = new Library();
+
 const domHandler = (function () {
   //code for the menu
   const dialog = document.querySelector("dialog#dialog-add");
   const dialogEdit = document.querySelector("dialog#dialog-edit");
   const showButton = document.querySelector("button.book-add");
-  const submitButton = document.querySelector(
-    `dialog#dialog-add button.submit`
-  );
-  const submitEditButton = document.querySelector(
-    `dialog#dialog-edit button.submit`
-  );
+  const submitButton = document.querySelector(`#dialog-add button.submit`);
+  const submitEditButton = document.querySelector(`#dialog-edit button.submit`);
   const closeButton = document.querySelector("dialog#dialog-add button.close");
-  const closeEditButton = document.querySelector(
-    "dialog#dialog-edit button.close"
-  );
+  const closeEditButton = document.querySelector("#dialog-edit button.close");
 
   const booksNode = document.querySelector("#book-grid-content");
 
@@ -23,12 +93,14 @@ const domHandler = (function () {
     const formData = new FormData(form);
     form.reset();
 
-    addBook(
-      formData.get("book-name"),
-      formData.get("book-author"),
-      formData.get("book-pages"),
-      formData.get("book-status"),
-      formData.get("book-rating")
+    myLibrary.addBook(
+      new Book(
+        formData.get("book-name"),
+        formData.get("book-author"),
+        formData.get("book-pages"),
+        formData.get("book-status"),
+        formData.get("book-rating")
+      )
     );
 
     dialog.close();
@@ -39,7 +111,7 @@ const domHandler = (function () {
     const form = document.getElementById("book-form-edit");
     const formData = new FormData(form);
 
-    editBook(
+    myLibrary.editBook(
       dialogEdit.dataset.index,
       new Book(
         formData.get("book-name"),
@@ -50,7 +122,7 @@ const domHandler = (function () {
       )
     );
 
-    saveLibrary();
+    myLibrary.saveLibrary();
     displayBooks();
 
     dialogEdit.close();
@@ -70,101 +142,43 @@ const domHandler = (function () {
   function displayBooks() {
     booksNode.innerHTML = "";
 
-    myLibrary.forEach((book, index) => {
+    for (let i = 0; i < myLibrary.length(); ++i) {
+      let book = myLibrary.getBook(i);
       const bookCard = document.createElement("div");
       bookCard.setAttribute("class", "book-row");
       bookCard.innerHTML = `
-    <div class="book-main">
-      <div class="book-name">${book.name} by ${book.author}</div>
-      <button>Edit</button>
-      <button data-index="${index}">x</button>
-    </div>
-    <div class="book-pages">${book.pages}pg</div>
-    <div class="book-status"><div>${book.status}</div></div>
-    <div class="book-rating">${book.rating}</div>`;
+      <div class="book-main">
+        <div class="book-name">${book.name} by ${book.author}</div>
+        <button>Edit</button>
+        <button data-index="${i}">x</button>
+      </div>
+      <div class="book-pages">${book.pages}pg</div>
+      <div class="book-status"><div>${book.status}</div></div>
+      <div class="book-rating">${book.rating}</div>`;
 
       const bookEdit = bookCard.querySelector("button");
       bookEdit.addEventListener("click", function () {
-        dialogEdit.querySelector("#name").value = myLibrary.at(index).name;
-        dialogEdit.querySelector("#author").value = myLibrary.at(index).author;
-        dialogEdit.querySelector("#pages").value = myLibrary.at(index).pages;
-        dialogEdit.querySelector("#status").value = myLibrary.at(index).status;
-        dialogEdit.querySelector("#rating").value = myLibrary.at(index).rating;
-        dialogEdit.setAttribute("data-index", index);
+        var book = myLibrary.getBook(i);
+        dialogEdit.querySelector("#name").value = book.name;
+        dialogEdit.querySelector("#author").value = book.author;
+        dialogEdit.querySelector("#pages").value = book.pages;
+        dialogEdit.querySelector("#status").value = book.status;
+        dialogEdit.querySelector("#rating").value = book.rating;
+        dialogEdit.setAttribute("data-index", i);
         dialogEdit.showModal();
       });
 
-      const bookRemove = bookCard.querySelector(
-        `button[data-index="${index}"]`
-      );
+      const bookRemove = bookCard.querySelector(`button[data-index="${i}"]`);
       bookRemove.addEventListener("click", function () {
-        removeBook(this.dataset.index);
+        myLibrary.removeBook(this.dataset.index);
       });
 
       booksNode.appendChild(bookCard);
-    });
+    }
   }
 
   return { displayBooks };
 })();
 
-const myLibrary = [];
-
-class Book {
-  // author, title, number of pages, whether it’s been read
-  constructor(name, author, pages, status, rating) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.rating = rating;
-  }
-}
-
-function addBook(name, author, pages, status, rating) {
-  myLibrary.push(new Book(name, author, pages, status, rating));
-  saveLibrary();
-  domHandler.displayBooks();
-}
-
-function getBook(index) {
-  return myLibrary.at(+index);
-}
-
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  saveLibrary();
-  domHandler.displayBooks();
-}
-
-function editBook(index, newBook) {
-  myLibrary.splice(index, 0, newBook);
-  myLibrary.splice(++index, 1);
-}
-
-function saveLibrary() {
-  localStorage.setItem("library", JSON.stringify(myLibrary));
-}
-
-function loadLibrary() {
-  let libraryVal = JSON.parse(localStorage.getItem("library"));
-  if (!libraryVal) return;
-
-  libraryVal.forEach((book) => {
-    myLibrary.push(book);
-  });
-}
-
-function addFluffBooks() {
-  addBook("The Enigmatic Elixir", "Penelope Puzzleton", 352, "false");
-  addBook("Secrets of the Starry Night", "Maxwell Moonshadow", 420, "true");
-  addBook("Whispers in the Wind", "Cassandra Mystique", 288, "false");
-  addBook("Lost in the Labyrinth", "Amelia Riddlewood", 416, "false");
-  addBook("The Mysterious Memoirs of Peculiar", "Edgar Enigma", 368, "true");
-  addBook("The Cosmic Conundrum", "Celeste Stardust", 512, "false");
-  addBook("The Puzzling Paradox", "Quentin Quizzleton", 276, "true");
-  addBook("The Cryptic Cipher", "Harrison Riddlestone", 400, "true");
-}
-
-loadLibrary();
+myLibrary.loadLibrary();
 domHandler.displayBooks();
